0ac66e64ddacb9e8c887c3baf475baa79df48036
Samuel Lin <samuelddlin@gmail.com>
2023-10-25 16:56:36 -0700

    add working array functions

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,11 +2,18 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
+	@Test
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+	}
+
+	@Test
+	public void testReverseInPlaceFail() {
+    int[] input1 = { 3, 0};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 0, 3 }, input1);
 	}
 
 
@@ -14,5 +21,11 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+	@Test
+  public void testReversedFail() {
+    int[] input1 = { 1, 2 };
+    assertArrayEquals(new int[]{ 2, 1}, ArrayExamples.reversed(input1));
   }
 }
